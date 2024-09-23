acc059d1593d636a86e900d81cb4f95798eae1a2
Kaue Portella <portellakaue@gmail.com>
2024-09-22 21:24:58 -0300

    Adicionado configuração de CORS.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173', // Specify the allowed origin
+    methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
+    credentials: true, // If you're using cookies or auth headers
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
